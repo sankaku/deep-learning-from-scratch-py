39de9e1aabafc7310e1f69bd805afd4a4b0d24e2
sankaku <sankaku.git@gmail.com>
2018-02-03 23:49:00 +0900

    add plot_overfitting(ch06)
    wierd graph...and too slow

---
+++ ch06/multi_layer_net.py
@@ -0,0 +1,157 @@
+# mod ch05.two_layer_net
+
+import sys
+import os
+sys.path.append(os.pardir)
+from ch03.sigmoid import sigmoid
+from ch03.softmax import softmax
+from ch04.numerical_gradient_batch import numerical_gradient_batch
+import numpy as np
+from ch05.ReluLayer import ReluLayer
+from ch05.SoftmaxWithLossLayer import SoftmaxWithLossLayer
+from collections import OrderedDict
+
+
+class AffineLayer:
+    """
+    forward/backward propagation for Affine transformation: Y = XW + B
+
+    Mod ch05.AffineLayer.
+    In MultiLayerNet.predict, ch05.AffineLayer is difficult to use
+    because its `forward` method needs arguments.
+    """
+
+    def __init__(self, W, b):
+        """
+        W: (n, m) matrix
+        b: (1, m) matrix. This is NOT (batch_size, m) matrix because bias is common for all data in `x`.
+        """
+        self.x = None
+        self.W = W
+        self.b = b
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        """
+        return y = xW + b
+
+        x: (batch_size, n) matrix
+        """
+        self.x = x
+        return np.dot(self.x, self.W) + self.b
+
+    def backward(self, doutdy):
+        """
+        return d(out)/dx, d(out)/dW
+
+        d(out)/dx = d(out)/dy * [W]T
+        d(out)/dW = [x]T * d(out)/dy
+        d(out)/db = d(out)/dy
+        ([A]T means the transpose matrix of A.)
+
+        doutdy: (batch_size, m) matrix (`batch_size` and `m` must be consistent with those of `forward`.)
+        """
+        doutdx = np.dot(doutdy, self.W.T)
+        self.dW = np.dot(self.x.T, doutdy)
+        self.db = np.sum(doutdy, axis=0)
+        return doutdx
+
+
+class MultiLayerNet:
+    def __init__(self, input_size, hidden_size_list, output_size, weight_init_std=0.01):
+        """
+        Initialize n-layers network.
+
+        Weight matrices are initialized randomly.
+        Biases are zero vectors.
+
+        input_size: the number of neurons in layer-0(input to this network)
+        hidden_size_list: the numbers of neurons in layer-1, 2, ..., n-1(hidden layers)
+        output_size: the number of neurons in layer-n(output of this network)
+        weight_init_std: weight to the weight matrices
+        """
+
+        self.hidden_size_list = hidden_size_list
+        hidden_layers_num = len(self.hidden_size_list)
+
+        # params
+        self.params = {}
+        all_size_list = [input_size] + self.hidden_size_list + [output_size]
+        for index in range(1, len(all_size_list)):
+            self.params['W{0}'.format(index)] = weight_init_std * \
+                np.random.randn(all_size_list[index - 1], all_size_list[index])
+            self.params['b{0}'.format(index)] = np.zeros(all_size_list[index])
+
+        # layers
+        self.layers = OrderedDict()
+        for index in range(1, hidden_layers_num + 1):
+            self.layers['Affine{0}'.format(index)] = AffineLayer(
+                self.params['W{0}'.format(index)], self.params['b{0}'.format(index)])
+            self.layers['Relu{0}'.format(index)] = ReluLayer()
+        self.layers['Affine{0}'.format(hidden_layers_num + 1)] = AffineLayer(self.params['W{0}'.format(
+            hidden_layers_num + 1)], self.params['b{0}'.format(hidden_layers_num + 1)])
+        self.lastLayer = SoftmaxWithLossLayer()
+
+    def predict(self, x):
+        """
+        x: input to this network(NumPy array)
+        """
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        """
+        x: input(NumPy array)
+        t: teacher data(NumPy array)
+        """
+        return self.lastLayer.forward(self.predict(x), t)
+
+    def accuracy(self, x, t):
+        """
+        Returns the accuracy of this network.
+
+        x: input(NumPy array)
+        t: teacher data(NumPy array)
+        """
+
+        y = self.predict(x)
+        y_labels = np.argmax(y, axis=1)
+        t_labels = np.argmax(t, axis=1)
+
+        num_all = x.shape[0]
+        num_hit = np.sum(y_labels == t_labels)
+        return num_hit / float(num_all)
+
+    def gradient(self, x, t):
+        """
+        Calculate gradient by backpropagation.
+        """
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        for index in range(1, len(self.hidden_size_list) + 2):
+            dW = self.layers['Affine{0}'.format(index)].dW
+            db = self.layers['Affine{0}'.format(index)].db
+            grads['W{0}'.format(index)] = dW
+            grads['b{0}'.format(index)] = db
+
+        return grads
+
+
+if __name__ == '__main__':
+    net = TwoLayerNet(784, 100, 10)
+    print("net.params['W1'].shape = {0}".format(net.params['W1'].shape))
+    print("net.params['b1'].shape = {0}".format(net.params['b1'].shape))
+    print("net.params['W2'].shape = {0}".format(net.params['W2'].shape))
+    print("net.params['b2'].shape = {0}".format(net.params['b2'].shape))
---
+++ ch06/plot_overfitting.py
@@ -0,0 +1,89 @@
+# Visualize overfitting
+# mod ch05.train_mnist_by_two_layer_net.py
+
+
+import sys
+import os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+from ch06.multi_layer_net import MultiLayerNet
+from ch06.SGD import SGD
+import matplotlib.pyplot as plt
+
+
+def train(batch_size, iterate_num, learning_rate):
+    """
+    MNIST training with too many network parameters and too small dataset
+
+    batch_size: data of this number are choosed from training data in each step
+    iterate_num: the number of iteration for backpropagation
+    learning_rate: learning rate for backpropagation
+    """
+    # get training data and test data(test data are not used below.)
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, one_hot_label=True)
+
+    epoch_size = 100
+
+    # reduce training dataset to overfit
+    x_train = x_train[:300]
+    t_train = t_train[:300]
+
+    # initialized MultiLayerNet
+    hidden_size_list = [100, 100]  # many layers to overfit
+    # each image has 28*28 pixels
+    network = MultiLayerNet(28 * 28, hidden_size_list, 10)
+
+    # accuracies
+    train_accuracies = []
+    test_accuracies = []
+
+    # optimizer
+    optimizer = SGD()
+
+    for i in range(iterate_num):
+        # choose the training data for this step
+        indices = np.random.choice(len(x_train), batch_size)
+        x_train_batch = x_train[indices]
+        t_train_batch = t_train[indices]
+
+        # calculate the grad
+        # grads = network.numerical_gradient(x_train_batch, t_train_batch)
+        grads = network.gradient(x_train_batch, t_train_batch)
+
+        # update the network parameters
+        optimizer.update(network.params, grads)
+
+        # record accuracy
+        if i % (iterate_num / epoch_size) == 0:
+            train_accuracy = network.accuracy(x_train, t_train)
+            test_accuracy = network.accuracy(x_test, t_test)
+            train_accuracies.append(train_accuracy)
+            test_accuracies.append(test_accuracy)
+            print('Done {0}/{1}. train_accuracy = {2}, test_accuracy = {3}'.format(
+                i + 1, iterate_num, train_accuracy, test_accuracy))
+
+    return network, train_accuracies, test_accuracies
+
+
+def plot_accuracies(train_accuracies, test_accuracies):
+    """
+    Plot `train_accuracies` and `test_accuracies`
+    """
+    x = np.arange(0, len(train_accuracies))
+    y = train_accuracies
+    plt.plot(x, y, label='train')
+    x = np.arange(0, len(test_accuracies))
+    y = test_accuracies
+    plt.plot(x, y, label='test')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    network, train_accuracies, test_accuracies = train(100, 10000, 0.1)
+    plot_accuracies(train_accuracies, test_accuracies)
