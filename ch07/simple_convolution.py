89a40225c86c4e45985d3a3e4f40949d931f495b
sankaku <sankaku.git@gmail.com>
2018-02-08 17:04:51 +0900

    impl simple_convolute

--- ch07/simple_convolution.py
+++ ch07/simple_convolution.py
@@ -22,8 +22,22 @@ def convolute(x, filters, padding=0, stride=1):
     if filters.shape[0] != channels:
         raise ValueError('Channel numbers of filters must be equal to that of x.: x.shape = {0}, filters.shape = {1}'.format(
             x.shape, filters.shape))
-    filter_heights = filters.shape[1]
+    filter_height = filters.shape[1]
     filter_width = filters.shape[2]
+
+    output_height, output_width = get_output_size(
+        height, width, filter_height, filter_width, padding, stride)
+    output = np.zeros([channels, output_height, output_width])
+
+    for oh_num in range(output_height):
+        for ow_num in range(output_width):
+            tmp_h = stride * oh_num
+            tmp_w = stride * ow_num
+            sliced = x[:, tmp_h:tmp_h + filter_height,
+                       tmp_w:tmp_w + filter_width]
+            output[:, oh_num, ow_num] = np.sum(
+                np.sum(sliced * filters, axis=1), axis=1)
+    return output
 
 
 def pad(x, padding):
@@ -36,3 +50,31 @@ def pad(x, padding):
     """
     pad_val = 0
     return np.pad(x, pad_width=[(0, 0), (padding, padding), (padding, padding)], mode='constant', constant_values=pad_val)
+
+
+def get_output_size(height, width, filter_height, filter_width, padding, stride):
+    """
+    return (output_height, output_width)
+
+    You can see below by drawing a figure.
+    height + 2 * padding = filter_height + (output_height - 1) * stride
+    width + 2 * padding = filter_width + (output_width - 1) * stride
+
+    height: height of input
+    width: width of input
+    filter_height: height of filter
+    filter_width: width of filter
+    padding: width to expand the edge of input
+    stride: Width to slide filter
+    """
+    output_height = 1 + (height + 2 * padding - filter_height) / stride
+    output_width = 1 + (width + 2 * padding - filter_width) / stride
+
+    if output_height != int(output_height):
+        raise ValueError(
+            'output_height({0}) must be integer.'.format(output_height))
+    if output_width != int(output_width):
+        raise ValueError(
+            'output_width({0}) must be integer.'.format(output_width))
+
+    return int(output_height), int(output_width)
--- ch07/test/test_simple_convolution.py
+++ ch07/test/test_simple_convolution.py
@@ -28,7 +28,60 @@ class TestSimpleConvolution(unittest.TestCase):
         x = np.array([[[1,  2], [3,  4]]])
         padding = 1
         actual = pad(x, padding)
-        expected = np.array([[[0, 0, 0, 0], [0, 1, 2, 0], [0, 3, 4, 0], [0, 0, 0, 0]]])
+        expected = np.array(
+            [[[0, 0, 0, 0], [0, 1, 2, 0], [0, 3, 4, 0], [0, 0, 0, 0]]])
+
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_get_output_size_1(self):
+        """
+        test with padding=1, stride=1
+        """
+        actual = get_output_size(5, 8, 3, 4, 1, 1)
+        expected = (5, 7)
+        self.assertEquals(actual, expected)
+
+    def test_get_output_size_2(self):
+        """
+        test with invalid values
+
+        In this case, output_height and output_width are not integer
+        and `ValueError` is raised.
+        """
+        expected = ValueError
+
+        with self.assertRaises(expected):
+            actual = get_output_size(5, 8, 3, 4, 1, 3)
+
+    def test_convolute_1(self):
+        """
+        simple test
+
+        x.shape == filters.shape
+        """
+        x = np.array([[[1,  2], [3,  4]],
+                      [[5,  6], [7,  8]]])
+        filters = np.array([[[1,  0], [2,  0]],
+                            [[3,  0], [4,  0]]])
+        actual = convolute(x, filters, padding=0, stride=1)
+        expected = np.array([[[7]], [[43]]])
+
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_convolute_2(self):
+        """
+        x.shape = (3, 4, 5)
+        filters.shape = (3, 2, 3)
+        """
+        x = np.arange(3 * 4 * 5).reshape(3, 4, 5)
+        filters = np.array([[[1,  0,  0], [0,  1,  0]],
+                            [[0,  0,  1], [1,  0,  0]],
+                            [[0,  1,  0], [0,  0,  1]]])
+
+        actual = convolute(x, filters, padding=0, stride=2)
+        expected = np.array([[[6,  10.], [26,  30]],
+                             [[47,  51], [67,  71]],
+                             [[88,  92], [108, 112]]])
 
         np.testing.assert_array_equal(actual, expected)
 
